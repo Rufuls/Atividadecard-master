d12bc63dfe97e88659e85ceb747e545822e953c6
GUILHERME BARBOSA ALVES <guilherme.balves3@senacsp.edu.br>
2019-05-15 12:18:55 -0300

    Initial commit

---
+++ app/src/main/java/sp/senac/br/atividadecard/MainActivity.java
@@ -0,0 +1,35 @@
+package sp.senac.br.atividadecard;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarmenu);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
---
+++ app/src/main/java/sp/senac/br/atividadecard/TempoActivity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.atividadecard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TempoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tempo_layout);
+    }
+}
